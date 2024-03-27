11d7c1b0a85ea074eb633c1c09db1ba2337d6cee
Lyuma <xn.lyuma@gmail.com>
2024-03-27 03:06:00 -0700

    Fix abstract and process_modification binding

--- scene/3d/skeleton_modifier_3d.cpp
+++ scene/3d/skeleton_modifier_3d.cpp
@@ -139,7 +139,7 @@ void SkeletonModifier3D::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_influence", "influence"), &SkeletonModifier3D::set_influence);
 	ClassDB::bind_method(D_METHOD("get_influence"), &SkeletonModifier3D::get_influence);
 
-	ClassDB::bind_method(D_METHOD("_process_modification"), &SkeletonModifier3D::_process_modification);
+	ClassDB::bind_method(D_METHOD("process_modification"), &SkeletonModifier3D::process_modification);
 
 	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "active"), "set_active", "is_active");
 	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "influence", PROPERTY_HINT_RANGE, "0,1,0.001"), "set_influence", "get_influence");
